1325ae7b0271e2ba34eb2b54b64ee41e91035cd4
Parcoltw <parcol0711@gmail.com>
2026-04-27 15:41:17 +0800

    change nu & rnu colors

--- lua/plugins/monokai.lua
+++ lua/plugins/monokai.lua
@@ -5,8 +5,10 @@ return {
   config = function()
     require("monokai").setup({
       custom_hlgroups = {
-        Visual    = { bg = "#7a7a7a", style = "bold" },
-        CurSearch = { fg = "#1e1e1e", bg = "#f29c38" },
+        Visual        = { bg = "#7a7a7a", style = "bold" },
+        CurSearch     = { fg = "#1e1e1e", bg = "#f29c38" },
+        LineNr        = { fg = "#aaaaaa" },
+        CursorLineNr  = { fg = "#fd971f" },
       }
     })
   end,
