93c084386ca390c65781d315f2dee9884d4bed18
Parcoltw <parcol0711@gmail.com>
2026-04-28 10:26:36 +0800

    add plugins

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,12 @@
+return {
+  "catgoose/nvim-colorizer.lua",
+  config = function()
+    require("colorizer").setup({
+      filetypes = { "*" },
+      user_default_options = {
+        RRGGBB = true,
+        names  = false,
+      },
+    })
+  end,
+}
--- lua/plugins/monokai.lua
+++ lua/plugins/monokai.lua
@@ -4,6 +4,7 @@ return {
   priority = 1000,
   config = function()
     require("monokai").setup({
+      italics = false,
       custom_hlgroups = {
         Visual = { bg = "#5c6370", style = "bold" },
         CurSearch     = { fg = "#1e1e1e", bg = "#f29c38" },
